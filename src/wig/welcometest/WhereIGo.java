d19ef3a66bf2c7987ac07cc5d748f7163b8abc41
AnaLacramioaraNastasescu <56443451+AnaLacramioaraNastasescu@users.noreply.github.com>
2019-11-13 18:57:05 +0100

    Link repository to Travis and SonarCloud -19 (try 2)

---
+++ src/wig/welcometest/WhereIGo.java
@@ -0,0 +1,11 @@
+package wig.welcometest;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WhereIGo {
+	private static final Logger LOGGER = Logger.getLogger( WhereIGo.class.getName() );
+	public static void main(String[] args) {
+		LOGGER.log( Level.INFO, "Welcome to WhereIGo!");
+	}
+}
